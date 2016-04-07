26c00a13aed1f5adf974168ce24ffd6c87641aa4
buttasam <samuel.butta@seznam.cz>
2016-04-07 13:05:07 +0200

    hibernate initial

---
+++ src/main/java/hibernate/AuthorEntity.java
@@ -0,0 +1,53 @@
+package hibernate;
+
+import javax.persistence.*;
+
+/**
+ * Created by samik on 6.4.16.
+ */
+@Entity
+@Table(name = "author", schema = "eja-library", catalog = "")
+public class AuthorEntity {
+    private int id;
+    private String name;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AuthorEntity that = (AuthorEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/hibernate/BookEntity.java
@@ -0,0 +1,53 @@
+package hibernate;
+
+import javax.persistence.*;
+
+/**
+ * Created by samik on 6.4.16.
+ */
+@Entity
+@Table(name = "book")
+public class BookEntity {
+    private int id;
+    private String title;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BookEntity that = (BookEntity) o;
+
+        if (id != that.id) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+}
